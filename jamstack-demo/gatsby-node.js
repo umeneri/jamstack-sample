d1346a569df2d3f5e5aa2e622e1682fd4c9797e1
Masato Koishi <masato.koishi@stanby.inc>
2020-05-09 18:44:46 +0900

    add pages

--- jamstack-demo/gatsby-node.js
+++ jamstack-demo/gatsby-node.js
@@ -5,3 +5,35 @@
  */
 
 // You can delete this file if you're not using it
+const path = require(`path`)
+
+exports.createPages = async gatsbyNodeHelpers => {
+  const { graphql, actions } = gatsbyNodeHelpers
+  const { createPage } = actions
+
+  console.log("createPage");
+
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.frontmatter.slug,
+      component: path.resolve(`./src/templates/post.js`),
+      context: {
+        slug: node.frontmatter.slug
+      }
+    })
+  })
+}
--- jamstack-demo/src/pages/index.js
+++ jamstack-demo/src/pages/index.js
@@ -1,21 +1,40 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Hello, Gatsby!</h1>
+    {data.allMarkdownRemark.edges.map(({ node }) => (
+      <article key={node.frontmatter.slug}>
+        <h2>
+          <Link to={`/${node.frontmatter.slug}`}>
+            {node.frontmatter.title}
+          </Link>
+        </h2>
+        <time dateTime={node.frontmatter.date}>{node.frontmatter.date}</time>
+        <p>{node.excerpt}</p>
+      </article>
+    ))}
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: ASC }) {
+      edges {
+        node {
+          frontmatter {
+            title
+            date(formatString: "YYYY年MM月DD日")
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
